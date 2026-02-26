343c9dad698d8f245868a16b730632acffac0437
Marija Nesic <nesicmarija123@gmail.com>
2026-02-26 13:33:37 +0100

    finished refactoring

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,22 +1,44 @@
 use std::env;
 //module to help us with module reading 
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect(); 
   
     //let(query, filename) = parse_config(&args);
     //let config = parse_config(&args);
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-   let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
-    println!("With text: \n{}", contents);
+    //prebaci ovo u posebnu funkciju
+   // let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+   //  println!("With text: \n{}", contents);
+
+   // run(config);
+
+   if let Err(e) = run(config){
+        println!("Application error: {}", e);
+        process::exit(1);
+   }
+
 
 }
+
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text: \n{}", contents);
+    Ok(())
+}
+
 
 struct Config{
     query: String,
